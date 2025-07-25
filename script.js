c5cd7e6c73369f110296189b520f18e7f4848205
LJPethig <76712563+LJPethig@users.noreply.github.com>
2025-07-25 21:46:46 +1200

    Final code
    Add instructions on how to initiate the game
    Refactor getHumanChoice function with do while loop and validation

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let humanScore;
 let computerScore;
 const ROUNDS = 5; /* Number of game rounds */
 
-function computerChoice() {
+function GetComputerChoice() {
     let num = Math.floor(Math.random() * 3) + 1;
     if (num === 1) {
         return "ROCK";
@@ -13,10 +13,39 @@ function computerChoice() {
     }
 }
 
-function humanChoice() {
-    return "ROCK";
+function getHumanChoice() {
+    /* Get user input
+       Validate input
+       Return valid choice
+    */
+    let validChoice
+    let userInput
+
+    do {
+        userInput = getUserInput();
+        validChoice = validateUserInput(userInput);
+        if (!validChoice) {
+            console.log("That was not a valid choice. Please enter ROCK, PAPER, or SCISSORS");
+        }
+    } while (!validChoice);
+    return validChoice;
 }
 
+function validateUserInput(userInput) {
+    if (userInput) {
+        userInput = userInput.toUpperCase();
+    }
+    if (userInput === "ROCK" || userInput === "PAPER" || userInput === "SCISSORS") {
+        return userInput;
+    } else {
+        return null;
+    }
+}
+
+function getUserInput() {
+    let userInput = prompt("Enter your choice (ROCK, PAPER, or SCISSORS)");
+    return userInput;
+}
 function roundWinner(humanChoice, computerChoice) {
     let result;
     if (computerChoice === humanChoice) {
@@ -35,12 +64,12 @@ function roundWinner(humanChoice, computerChoice) {
 }
 
 function roundResult(humanChoice, computerChoice, result) {
-    return `Computer choose ${computerChoice}, you choose ${humanChoice} ${result}`;
+    return `Computer chose ${computerChoice}, you chose ${humanChoice} ${result}`;
 }
 
 function playRound(roundNumber) {
     console.log(`Round ${roundNumber}`);
-    console.log(roundWinner(humanChoice(), computerChoice()));
+    console.log(roundWinner(getHumanChoice(), GetComputerChoice()));
 }
 
 function gameWinner() {
@@ -64,6 +93,8 @@ function playGame() {
     console.log(gameWinner());
 }
 
-console.log("Welcome to Rock, Paper, Scissors Game");
-playGame();
+/* This prevents the game automatically starting and prompt()
+   blocking opening dev tools console */
+alert("Welcome to Rock, Paper, Scissors. Click Close then open the console");
+console.log("Welcome to Rock, Paper, Scissors Game. Enter playGame() to begin");
 
